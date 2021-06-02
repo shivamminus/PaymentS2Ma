6f81afc668a95c0335d23adadaecb72e0971c09f
shivamminus <dubeyshivam044@gmail.com>
2021-06-02 17:38:16 +0530

    deploy push

--- src/main/java/com/payment/main/PaymentS2Ma1Application.java
+++ src/main/java/com/payment/main/PaymentS2Ma1Application.java
@@ -4,13 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient
+//@EnableEurekaClient
 public class PaymentS2Ma1Application {
 	
 	private static Logger logger = LoggerFactory.getLogger(PaymentS2Ma1Application.class);
--- src/main/java/com/payment/main/client/AuthClient.java
+++ src/main/java/com/payment/main/client/AuthClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import com.payment.main.dao.ValidatingDAO;
 
 //@FeignClient(name = "AUTH-SERVICE", url = "http://localhost:9090/auth")
-@FeignClient(name = "AUTH-SERVICE", url = "http://localhost:8081/auth")
+@FeignClient(name = "AUTH-SERVICE", url = "http://s2ma-pod3.us-east-2.elasticbeanstalk.com/auth")
 
 public interface AuthClient {
 
